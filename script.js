1d7e078fc9a5a8b70257d0a58a43600c0ba163b2
jackiescognamiglio <111523756+jackiescognamiglio@users.noreply.github.com>
2023-12-05 15:28:59 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     function shareOnFacebook() {
         const urlToShare= 'https://jackiescognamiglio.github.io/jackie_s.github.io/';
         const facebookShareUrl=`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
-        window.open(facebookShareUrl, 'Share on Facebook', 'width=600, height=400')
+        window.open(facebookShareUrl, 'Share on Facebook', 'width=700, height=500')
     }
 
     function shareOnTwitter() {
         const urlToShare='https://jackiescognamiglio.github.io/jackie_s.github.io/'
         const textToShare='Click here!';
         const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
-        window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
+        window.open(twitterShareUrl, 'Share on Twitter', 'width=700,height=500');
     }
 
     const shareFacebookButton=document.getElementById("share-facebook");
